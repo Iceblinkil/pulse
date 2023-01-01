5d5b09248894594fd8a7fef5234a175ae4c988cd
Iceblink <semenkrik@gmail.com>
2023-01-01 22:48:10 +0200

    lesson 57.4

--- src/js/script.js
+++ src/js/script.js
@@ -53,8 +53,8 @@ const slider = tns({
   
 $(document).ready(function(){
 	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-		$(this)
-		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-		  .closest('div.container').find('div.catalog__content').removeClass('catalog__tab_active').eq($(this).index()).addClass('catalog__tab_active');
-	  });
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 });
